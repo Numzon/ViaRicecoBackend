8388dd186c02ddd5e4accfd13efb5fb91003639d
numzon <mlynarczyk@protonmail.com>
2025-09-29 18:24:59 +0200

    Introduce `MonthlyBudgetExpense` aggregate in Budgets domain with nullable decimal value property.

---
+++ ViaRiceco.Modules.Budgets.Domain/MonthlyBudgetExpenses/MonthlyBudgetExpense.cs
@@ -0,0 +1,7 @@
+﻿namespace ViaRiceco.Modules.Budgets.Domain.MonthlyBudgetExpenses;
+
+public sealed class MonthlyBudgetExpense
+{
+    //realtion between expense and monthly budget
+    //additional value property - decimal, that can be nullable
+}
